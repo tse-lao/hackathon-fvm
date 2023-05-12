7a6e08f6c2243bc1ab1ef5a95c595a27e608229e
Nick Lionis <nicklioniss@gmail.com>
2023-05-12 12:26:59 +0300

    new tables in contants

--- src/constants/tableland.js
+++ src/constants/tableland.js
@@ -1,4 +1,4 @@
 export const computation = "computation_3141_15"
-export const data_contribution = "data_contribution_80001_6089 "
-export const DB_main = "file_main_80001_6090"
-export const DB_attribute = "file_attribute_80001_6091"
+export const data_contribution = "data_contribution_80001_6093 "
+export const DB_main = "file_main_80001_6094"
+export const DB_attribute = "file_attribute_80001_6095"
